e6e83c76a38842602e1b52f16d7fee77e27287c8
Delpod <xvix56@gmail.com>
2022-10-01 13:09:47 +0200

    Movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    [SerializeField] Character[] characterList;
+
     private Vector2 movingVector;
+    private Vector2 lookingVector;
+    private float currentMaxSpeed = 5f;
 
     private void Start() {
-        
     }
 
     private void OnDestroy() {
-        
     }
 
     private void Update() {
-        if () {
+        movingVector = InputManager.instance.playerInput.actions[Strings.PLAYER_MOVE].ReadValue<Vector2>();
+        lookingVector = InputManager.instance.playerInput.actions[Strings.PLAYER_LOOK].ReadValue<Vector2>();
 
+        transform.position += currentMaxSpeed * Time.deltaTime * new Vector3(movingVector.x, movingVector.y);
+
+        if (InputManager.instance.playerInput.actions[Strings.PLAYER_FIRE].triggered) {
+            // Shooting
         }
     }
 }
